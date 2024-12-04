cb4f752aa44471d6e9e685b2be0f8f02b2300e1d
Andrey Dembskiy-Novikov <dembskiynovikov@gmail.com>
2024-12-04 20:22:13 +0300

    step 1 update

---
+++ gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.');
+
+program 
+    .version('1.0.0', '-V, --version', 'output the version number');
+
+program 
+    .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
